ed42776fda57ff4082f4bd40bed8f5fc7c1a1862
michaellyon84 <michaellyon84@gmail.com>
2026-04-10 19:36:24 -0500

    Update PicksTab.tsx

--- components/tabs/PicksTab.tsx
+++ components/tabs/PicksTab.tsx
@@ -35,7 +35,7 @@ export function PicksTab({
   const weeks = Array.from({ length: currentWeek }, (_, i) => i + 1)
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-5">
       <WeekSwitcher
         selectedWeek={selectedWeek}
         onSelectWeek={setSelectedWeek}
